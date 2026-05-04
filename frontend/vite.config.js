c40c88aefb301dc51d45b8df7e4798b372830e72
Ignacio Sánchez <sanchez.ign1995@gmail.com>
2026-05-04 15:35:14 -0300

    Update vite.config.js
    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',                    // ← Muy importante para Vercel
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
+  }
 })
