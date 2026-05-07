44b64fb4c7b3c62a1ac1509ed8d24e150ab00389
Ignacio Sánchez <sanchez.ign1995@gmail.com>
2026-05-07 19:42:21 -0300

    Update shop.jsx
    Update shop.jsx

--- frontend/src/pages/shop.jsx
+++ frontend/src/pages/shop.jsx
@@ -11,10 +11,10 @@ const Shop = () => {
   const { auth } = useContext(AuthContext);
   const [shop, setShop] = useState(null);
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const fetchShop = useCallback(async () => {
     try {
-      console.log("shopid ", shopId)
       const response = await api.get(`/shops/${shopId}`);
       setShop(response.data);
     } catch (error) {
@@ -25,15 +25,15 @@ const Shop = () => {
   const fetchProducts = useCallback(async () => {
     try {
       const response = await api.get(`/products/by/${shopId}`);
-      console.log("QUIERO MIS PRODUCTOS: ", response)
       setProducts(response.data);
     } catch (error) {
       console.error('Error fetching products:', error);
+    } finally {
+      setIsLoading(false);
     }
   }, [shopId]);
 
   useEffect(() => {
-    console.log(auth.user?.shop_id, shopId)
     fetchShop();
     fetchProducts();
   }, [fetchShop, fetchProducts, auth.user?.shop_id, shopId]);
@@ -43,14 +43,19 @@ const Shop = () => {
   return (
     <Layout>
       <SearchBar />
-      <div style={{ backgroundImage: `url(${shop.cover_url})` }} alt="Cover" className="hero-image" ></div>
+      <div style={{ backgroundImage: `url(${shop.cover_url})` }} alt="Cover" className="hero-image"></div>
       <main className="p-10 pt-14 sm:pt-20 min-h-screen max-w-screen-2xl mx-auto relative">
-        <img src={shop.logo_url} alt="Logo" className="w-24 -top-12 sm:w-36 sm:-top-20  aspect-square object-cover rounded-full  absolute z-10 bg-neutral-300" />
-        {auth.user?.shop_id == shopId && <Link to={'/create-product'} className="p-3 py-2 top-2 right-10 rounded-3xl  absolute z-10 bg-secondary hover:bg-yellow-500" >Crear Producto</Link>}        <div className="mb-4">
+        <img src={shop.logo_url} alt="Logo" className="w-24 -top-12 sm:w-36 sm:-top-20 aspect-square object-cover rounded-full absolute z-10 bg-neutral-300" />
+        {auth.user?.shop_id == shopId && (
+          <Link to={'/create-product'} className="p-3 py-2 top-2 right-10 rounded-3xl absolute z-10 bg-secondary hover:bg-yellow-500">
+            Crear Producto
+          </Link>
+        )}
+        <div className="mb-4">
           <h2 className="text-3xl font-bold">{shop.name}</h2>
           <details className="cursor-pointer">
             <summary className="mt-2">
-              <span> {shop.description} </span>
+              <span>{shop.description}</span>
               <span className="pl-10 text-sm text-yellow-500">más&nbsp;información</span>
             </summary>
             <p className="mt-2"><strong>Teléfono:</strong> {shop.phone}</p>
@@ -58,7 +63,7 @@ const Shop = () => {
           </details>
         </div>
         <hr className="mt-6 mb-6 border-yellow-500" />
-        <ProductList products={products} />
+        <ProductList products={products} isLoading={isLoading} />
       </main>
     </Layout>
   );
